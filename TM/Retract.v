439d54b15a201ca1ae159f4252bf022589ff6c1b
Maximilian Wuttke <mwuttke97@gmail.com>
2017-11-18 23:30:59 +0100

    Retract.v: Verallgemeinerung mithilfe von Bijektionen

--- TM/Retract.v
+++ TM/Retract.v
@@ -1,60 +1,178 @@
+(* Library for injections, bijections, retracts and tight retracts *)
 Require Import Shared.Base.
 
 (*
  * This tactic tries to prove derived injectivity or (tight) retracts
-*)
-
-(*
-Ltac auto_inj := repeat (eauto with inj; autorewrite with inj).
-*)
-
+ *)
 Tactic Notation "auto_inj" := repeat (eauto with inj).
 Tactic Notation "auto_inj" integer(k) := repeat (eauto k with inj).
 
-Section Injection.
+(* Hint database for injections, bijections, retracts and tight retracts *)
+Create HintDb inj.
 
+
+Section Bijection.
   Variable X Y : Type.
+  Variable (f : X -> Y) (g : Y -> X).
 
-  Definition injective (f : X -> Y) := forall x1 x2, f x1 = f x2 -> x1 = x2.
-  Existing Class injective.
+  Definition left_inverse  := forall x : X, g (f x) = x.
+  Definition right_inverse := forall y : Y, f (g y) = y.
+  Definition inverse := left_inverse /\ right_inverse.
 
-  Definition inj_injective {f : X -> Y} {I : injective f} : forall x1 x2, f x1 = f x2 -> x1 = x2 := I.
+  Existing Class inverse.
 
-End Injection.
+  Hypothesis I : inverse.
+  Definition Inverse : inverse := I.
+  Definition Inverse_left  : left_inverse  := ltac:(now apply I).
+  Definition Inverse_right : right_inverse := ltac:(now apply I).
+End Bijection.
 
-Hint Unfold injective : inj.
+(* With the help of the Inverse* functions, the inversion proof should be automaticly infered *)
+Arguments Inverse       { X } { Y } f g { I }.
+Arguments Inverse_left  { X } { Y } f g { I }.
+Arguments Inverse_right { X } { Y } f g { I }.
 
-Ltac inj_subst :=
+(* Replace [ f (g x) ] with [ x ], etc. *)
+Ltac inverse :=
   match goal with
-  | [ H : ?t ?x = ?t ?y |- _] => eapply inj_injective in H; [ subst | now auto_inj]
+  | [ H : context [ ?f (?g _ ) ] |- _] =>
+    first [
+        (erewrite Inverse_left in H ; [ | now auto_inj ])
+        ||
+        (erewrite Inverse_right in H; [ | now auto_inj ])
+      ]
+  | [   |- context [ ?f (?g _ ) ]    ] =>
+    first [
+        (erewrite Inverse_left      ; [ | now auto_inj ])
+        ||
+        (erewrite Inverse_right     ; [ | now auto_inj ])
+      ]
   end.
 
-Instance injection_id (X : Type) : injective (@id X) := ltac:(unfold id; firstorder).
 
-Section Injection_Compose.
-  Variable X Y Z : Type.
-  Variable (f : X -> Y) (g : Y -> Z).
-  Hypothesis (f_inj : injective f) (g_inj : injective g).
-
-  Definition compose_inj : X -> Z := fun x => g (f x).
-
-  Instance compose_inj_injective : injective compose_inj := ltac:(firstorder).
-
-End Injection_Compose.
-
-Section Map_Injective.
-  Variable (sig tau : Type) (t : sig -> tau).
-  Hypothesis t_injective : injective t.
-
-  Instance map_injective :
-    injective (map t).
+Section Bijection_Test.
+  Variable X Y : Type.
+  Variable (f : X -> Y) (g : Y -> X).
+  Hypothesis I : inverse f g.
+  Goal forall x, g (f x) = x.
   Proof.
-    hnf. intros xs. induction xs; intros ys H; cbn in *.
-    - erewrite map_eq_nil; eauto.
-    - destruct ys; cbn in *; inv H. inj_subst. f_equal. auto.
+    intros x. inverse. reflexivity.
   Qed.
 
-End Map_Injective.
+  Goal forall y, f (g y) = y.
+  Proof.
+    intros y. inverse. reflexivity.
+  Qed.
+
+End Bijection_Test.
+
+
+
+Section Useful_Inversions.
+
+  Variable A B C D : Type.
+
+  (* Bijectivity is a euqivialance relation. *)
+  Section Inverse_Equivialence.
+    Instance inverse_id : inverse (@id A) (@id A).
+    Proof. hnf. firstorder. Qed.
+
+    Section Inverse_Comp.
+
+      Variable (f1 : A -> B) (g1 : B -> A) (f2 : B -> C) (g2 : C -> B).
+
+      Definition inverse_comp_f : A -> C := fun a => f2 (f1 a).
+      Definition inverse_comp_g : C -> A := fun c => g1 (g2 c).
+
+      Instance inverse_comp  :
+        inverse f1 g1 ->
+        inverse f2 g2 ->
+        inverse inverse_comp_f inverse_comp_g.
+      Proof.
+        unfold inverse_comp_f, inverse_comp_g.
+        intros (H1&H1') (H2&H2'). hnf in *. split; hnf.
+        - intros x. rewrite H2. rewrite H1. reflexivity.
+        - intros c. rewrite H1'. rewrite H2'. reflexivity.
+      Qed.
+
+    End Inverse_Comp.
+  
+    Instance inverse_symmetric (f : A -> B) (g : B -> A) :
+      inverse f g ->
+      inverse g f.
+    Proof. firstorder. Qed.
+
+  End Inverse_Equivialence.
+
+  Section Inverse_Sum.
+    Variable (f1 : A -> C) (g1 : C -> A) .
+    Variable (f2 : B -> D) (g2 : D -> B) .
+
+    Definition inverse_sum_f : A + B -> C + D :=
+      fun x => match x with
+            | inl a => inl (f1 a)
+            | inr c => inr (f2 c)
+            end.
+
+    Definition inverse_sum_g : C + D -> A + B :=
+      fun y => match y with
+            | inl c => inl (g1 c)
+            | inr d => inr (g2 d)
+            end.
+
+    Instance inverse_sum :
+      inverse f1 g1 -> 
+      inverse f2 g2 ->
+      inverse inverse_sum_f inverse_sum_g.
+    Proof. intros H1 H2. unfold inverse_sum_f, inverse_sum_g. hnf. split; hnf; intros [a|b]; now inverse. Qed.
+
+  End Inverse_Sum.
+
+  Section Inverse_Sum_Swap.
+
+    Definition inverse_sum_swap_f : A + B -> B + A :=
+      fun x =>
+        match x with
+        | inl a => inr a
+        | inr b => inl b
+        end.
+
+    Definition inverse_sum_swap_g : B + A -> A + B :=
+      fun x =>
+        match x with
+        | inl b => inr b
+        | inr a => inl a
+        end.
+
+    Instance inverse_sum_swap : inverse inverse_sum_swap_f inverse_sum_swap_g.
+    Proof. unfold inverse_sum_swap_f, inverse_sum_swap_g. hnf. split; hnf; intros [a|b]; reflexivity. Qed.
+
+  End Inverse_Sum_Swap.
+  
+  Section Inverse_Sum_Empty_set.
+
+    Definition inverse_sum_Empty_set_f : A + Empty_set -> A :=
+      fun x =>
+        match x with
+        | inl a => a
+        | inr b => match b with end
+        end.
+
+    Definition inverse_sum_Empty_set_g : A -> A + Empty_set := inl.
+    
+    Instance inverse_sum_Empty_set : inverse inverse_sum_Empty_set_f inverse_sum_Empty_set_g.
+    Proof. unfold inverse_sum_Empty_set_f, inverse_sum_Empty_set_g. hnf. split; hnf. now intros [ a | [] ]. tauto. Qed.
+
+  End Inverse_Sum_Empty_set.
+  
+End Useful_Inversions.
+
+Hint Resolve inverse_comp          : inj.
+Hint Resolve inverse_id            : inj.
+Hint Resolve inverse_symmetric     : inj.
+Hint Resolve inverse_sum_Empty_set : inj.
+Hint Resolve inverse_sum_swap      : inj.
+Hint Resolve inverse_symmetric     : inj.
 
 
 Section Retract.
@@ -149,20 +267,35 @@ End Retract_Compose.
 
 Hint Resolve tretract_compose : inj.
 
+Section Inversion_Retract.
+  Variable A B : Type.
+  Variable (f : A -> B) (g : B -> A).
+  
+  Instance inversion_retract :
+    inverse f g ->
+    tight_retract f (fun b => Some (g b)).
+  Proof.
+    intros H. hnf. intros a b. split.
+    - intros H2. inv H2.  now inverse.
+    - intros ->. f_equal. now inverse.
+  Qed.
+  
+End Inversion_Retract.
+
+Hint Resolve inversion_retract : inj.
 
 Section Usefull_Retracts.
 
   Variable (A B C D : Type).
 
+  (*
   Instance retract_id : tight_retract (@id A) (@Some A) := ltac:(unfold id; firstorder congruence).
+  *)
 
-  Instance retract_option : tight_retract (@Some A) id := ltac:(hnf; firstorder).
+  (* This can be derived, because [ id ] is a inversion with itself. *)
+  Instance retract_id : tight_retract (@id A) (@Some A) := ltac:(now auto_inj).
 
-  Instance retract_injective (f : A -> B) (g : B -> option A) :
-    retract f g -> injective f.
-  Proof.
-    intros H. intros x1 x2 H2. eapply retract_f_injective; eauto.
-  Qed.
+  Instance retract_option : tight_retract (@Some A) id := ltac:(now auto_inj).
 
   Definition retract_inl_f := @inl A B.
   Definition retract_inl_g := fun z : A + B => match z with inl x => Some x | inr _ => None end.
@@ -228,23 +361,76 @@ Section Usefull_Retracts.
     Qed.
 
   End RetractSum.
-    
+
 End Usefull_Retracts.
 
-Hint Resolve retract_injective : inj.
 Hint Resolve retract_id        : inj.
 Hint Resolve retract_option    : inj.
 Hint Resolve retract_inl       : inj.
 Hint Resolve retract_inr       : inj.
 Hint Resolve tretract_sum      : inj.
 
+
+Section Injection.
+
+  Variable X Y : Type.
+
+  Definition injective (f : X -> Y) := forall x1 x2, f x1 = f x2 -> x1 = x2.
+  Existing Class injective.
+
+  Definition inj_injective {f : X -> Y} {I : injective f} : forall x1 x2, f x1 = f x2 -> x1 = x2 := I.
+
+End Injection.
+
+Ltac inj_subst :=
+  match goal with
+  | [ H : ?t ?x = ?t ?y |- _] => eapply inj_injective in H; [ subst | now auto_inj]
+  end.
+
+Instance injection_id (X : Type) : injective (@id X) := ltac:(unfold id; firstorder).
+
+Section Injection_Compose.
+  Variable X Y Z : Type.
+  Variable (f : X -> Y) (g : Y -> Z).
+  Hypothesis (f_inj : injective f) (g_inj : injective g).
+
+  Definition compose_inj : X -> Z := fun x => g (f x).
+
+  Instance compose_inj_injective : injective compose_inj := ltac:(firstorder).
+
+End Injection_Compose.
+
+
+Section Map_Injective.
+  Variable (sig tau : Type) (t : sig -> tau).
+  Hypothesis t_injective : injective t.
+
+  Instance map_injective :
+    injective (map t).
+  Proof.
+    hnf. intros xs. induction xs; intros ys H; cbn in *.
+    - erewrite map_eq_nil; eauto.
+    - destruct ys; cbn in *; inv H. inj_subst. f_equal. auto.
+  Qed.
+
+End Map_Injective.
+
+Instance retract_injective (A B : Type) (f : A -> B) (g : B -> option A) :
+  retract f g -> injective f.
+Proof.
+  intros H. intros x1 x2 H2. eapply retract_f_injective; eauto.
+Qed.
+Hint Resolve retract_injective : inj.
+
 Section Injection_Corollaries.
   Variable A B : Type.
   
+  Instance injective_id : injective (@id A) := ltac:(now auto_inj).
   Instance injective_inl : injective (@inl A B) := ltac:(now auto_inj).
   Instance injective_inr : injective (@inr A B) := ltac:(now auto_inj).
   
 End Injection_Corollaries.
 
+Hint Resolve injective_id : inj.
 Hint Resolve injective_inl : inj.
 Hint Resolve injective_inr : inj.
