a67cd35e23cd8f7059472b3fa21150f1e868c9ec
Maximilian Wuttke <mwuttke97@gmail.com>
2017-12-09 08:14:36 +0100

    WriteString with parameter

--- TM/Basic/WriteString.v
+++ TM/Basic/WriteString.v
@@ -4,11 +4,12 @@ Section Write_String.
 
   Variable sig : finType.
   Variable D : move.
+  Variable (F : finType) (f : F).
 
-  Fixpoint Write_String (l : list sig) : {M : mTM sig 1 & states M -> unit} :=
+  Fixpoint Write_String (l : list sig) : {M : mTM sig 1 & states M -> F} :=
     match l with
-    | [] => mono_Nop sig tt
-    | e :: l0 => Write e tt ;; Move _ D tt ;; Write_String l0
+    | [] => mono_Nop sig f
+    | x :: xs => Write x tt ;; Move _ D tt ;; Write_String xs
     end.
 
   Fixpoint Tape_Write_String (t : tape sig) (str : list sig) :=
@@ -21,9 +22,9 @@ Section Write_String.
     Tape_Write_String t nil = t.
   Proof. destruct t; cbn; auto. Qed.
 
-  Fixpoint Write_string_sem_fix (str : list sig) : Rel (tapes sig 1) (unit * tapes sig 1) :=
+  Fixpoint Write_string_sem_fix (str : list sig) : Rel (tapes sig 1) (F * tapes sig 1) :=
     match str with
-    | nil => mono_Nop_R tt
+    | nil => mono_Nop_R f
     | s :: str' => Write_R s tt ∘ hideParam (Move_R D tt ∘ hideParam (Write_string_sem_fix str'))
     end.
     
@@ -43,16 +44,14 @@ Section Write_String.
   Qed.
   
   Definition Write_String_R str :=
-    Mono.Mk_R_p (F := FinType (EqType unit))
-                (ignoreParam (fun (t t' : tape sig) => t' = Tape_Write_String t str)).
-
+    Mono.Mk_R_p (fun tin '(yout, tout) => tout = Tape_Write_String tin str /\ yout = f).
   Lemma Write_string_Sem str :
     Write_String str ⊨c(4 * |str|) (Write_String_R str).
   Proof.
     eapply RealiseIn_monotone with (k1 := 4 * (|str|)); [now apply Write_string_fix_Sem | omega | ]. 
     induction str.
-    - hnf. intros x ([], y) (H1 & H2). hnf in *. subst. now rewrite Tape_Write_String_nil.
-    - intros t1 ([], t2) (([]&t3)&H1&((b&t4)&H2&H3)). hnf in *. destruct H1 as [_ H1]. destruct H2 as [_ H2]. hnf in *.
+    - hnf. intros x (f', y) (H1 & H2). hnf in *. subst. now rewrite Tape_Write_String_nil.
+    - intros t1 (f', t2) (([]&t3)&H1&((b&t4)&H2&H3)). hnf in *. destruct H1 as [_ H1]. destruct H2 as [_ H2]. hnf in *.
       destruct_tapes. cbn in *. cbn. subst. specialize (IHstr _ _ H3). clear H3. cbn in IHstr. hnf in IHstr. auto.
   Qed.
 
