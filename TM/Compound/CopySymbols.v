f11a447740877205ae6bfdf19b2270c9003f76d7
Maximilian Wuttke <mwuttke97@gmail.com>
2018-02-18 20:42:37 +0100

    Move und Copy alternative Korrektheitslemmas in alternative Relation untergebracht
    Die Automatisierung wurde entsprechend erweitert
    Die Maschinen in TM/Code/Match* müssen noch angepasst werden

--- TM/Compound/CopySymbols.v
+++ TM/Compound/CopySymbols.v
@@ -17,7 +17,7 @@ Section CopySymbols.
   
   Variable sig : finType.
   (* Termination check *)
-  Variable f : sig -> bool.
+  Variable stop : sig -> bool.
   (* translation *)
   Variable g : sig -> sig.
 
@@ -27,7 +27,7 @@ Section CopySymbols.
              match b with
              | Some x =>
                (* First write the read symbol to tape 1 *)
-               if f x
+               if stop x
                then (* found the symbol: write it to tape 1; break and return true *)
                  Inject (Write (g x) (false, tt)) [|Fin.FS Fin.F1|]
                else (* wrong symbol: write it to tape 1 and move both tapes right and continue *)
@@ -40,7 +40,7 @@ Section CopySymbols.
     fun '(t1, t2) =>
       match t1, t2 with
       | midtape ls x rs as t1, t2 =>
-        if (f x)
+        if (stop x)
         then (t1, tape_write t2 (Some (g x)))
         else (tape_move_right t1, tape_move_mono t2 (Some (g x), R))
       | t1, t2 => (t1, t2)
@@ -49,7 +49,7 @@ Section CopySymbols.
 (*
 End CopySymbols.
 Section Test.
-  Let f := fun x => Dec (x = L) : bool.
+  Let stop := fun x => Dec (x = L) : bool.
   Compute it (M1_Fun f) 0 (midtape [L; N; R] N [R; N; L; N], niltape _).
   Compute it (M1_Fun f) 1 (midtape [L; N; R] N [R; N; L; N], niltape _).
   Compute it (M1_Fun f) 2 (midtape [L; N; R] N [R; N; L; N], niltape _).
@@ -72,8 +72,8 @@ End Test.
     (fun tin '(yout, tout) =>
        (tout[@Fin.F1], tout[@Fin.FS Fin.F1]) = M1_Fun (tin[@Fin.F1], tin[@Fin.FS Fin.F1]) /\
        (
-         (yout = (false, tt) /\ exists s, current tin[@Fin.F1] = Some s /\ f s = true ) \/
-         (yout = (true,  tt) /\ exists s, current tin[@Fin.F1] = Some s /\ f s = false) \/
+         (yout = (false, tt) /\ exists s, current tin[@Fin.F1] = Some s /\ stop s = true ) \/
+         (yout = (true,  tt) /\ exists s, current tin[@Fin.F1] = Some s /\ stop s = false) \/
          (yout = (false, tt) /\ current tin[@Fin.F1] = None)
        )
     ).
@@ -87,9 +87,9 @@ End Test.
     eapply RealiseIn_monotone.
     {
       unfold M1. eapply Match_RealiseIn. cbn. eapply Inject_RealisesIn; [vector_dupfree| eapply read_char_sem].
-      instantiate (2 := fun o : option sig => match o with Some x => if f x then _ else _ | None => _ end).
+      instantiate (2 := fun o : option sig => match o with Some x => if stop x then _ else _ | None => _ end).
       intros [ | ]; cbn.
-      - destruct (f e); swap 1 2.
+      - destruct (stop e); swap 1 2.
         + eapply Seq_RealiseIn. eapply Inject_RealisesIn; [vector_dupfree | eapply Write_Sem]. eapply MovePar_Sem.
         + cbn. eapply Inject_RealisesIn; [vector_dupfree | eapply Write_Sem].
       - cbn. eapply RealiseIn_monotone'. eapply Nop_total. omega.
@@ -124,7 +124,7 @@ End Test.
   Function CopySymbols_Fun (tin : tape sig * tape sig) { measure rlength' tin } : tape sig * tape sig :=
     match tin with
       (midtape ls m rs as t1, t2) =>
-      if f m
+      if stop m
       then (t1, tape_write t2 (Some (g m)))
       else CopySymbols_Fun (tape_move_right t1, tape_move_mono t2 (Some (g m), R))
     |  (t1, t2) => (t1, t2)
@@ -139,8 +139,8 @@ End Test.
 (* (* Test *)
 End CopySymbols.
 Section Test.
-  Let f := fun x => Dec (x = L) : bool.
-  Compute CopySymbols_Fun f (midtape [L; N; R] N [R; N; L; N], niltape _).
+  Let stop := fun x => Dec (x = L) : bool.
+  Compute CopySymbols_Fun stop (midtape [L; N; R] N [R; N; L; N], niltape _).
   Compute it (M1_Fun f) 4 (midtape [L; N; R] N [R; N; L; N], niltape _).
 End Test.
 *)
@@ -162,7 +162,7 @@ End Test.
 
   Lemma M1_true t x :
     current t = Some x ->
-    f x = true ->
+    stop x = true ->
     M1_Fun t = t.
   Proof.
     intros H1 H2. unfold M1_Fun. destruct t; cbn in *; inv H1. rewrite H2. auto.
@@ -170,7 +170,7 @@ End Test.
   
   Lemma M1_Fun_M2_true t x :
     current t = Some x ->
-    f x = true ->
+    stop x = true ->
     MoveToSymbol_Fun t = M1_Fun t.
   Proof.
     intros H1 H2. destruct t; cbn in *; inv H1. rewrite MoveToSymbol_Fun_equation, H2. auto.
@@ -178,7 +178,7 @@ End Test.
 
   Lemma MoveToSymbol_M1_false t x :
     current t = Some x ->
-    f x = false ->
+    stop x = false ->
     MoveToSymbol_Fun (M1_Fun t) = MoveToSymbol_Fun t.
   Proof.
     intros H1 H2. functional induction MoveToSymbol_Fun t; cbn.
@@ -218,7 +218,7 @@ End Test.
           + inv H. inv H1. rewrite CopySymbols_Fun_equation. rewrite H2''. cbn. auto.
           + inv H. inv H1. rewrite CopySymbols_Fun_equation. rewrite H2''. cbn. auto.
           + inv H. inv H1. rewrite CopySymbols_Fun_equation. rewrite H2''. cbn. auto.
-          + destruct (f e) eqn:E; cbn in *.
+          + destruct (stop e) eqn:E; cbn in *.
             * inv H. destruct l0; cbn in *; inv H1; auto; rewrite CopySymbols_Fun_equation; cbn; rewrite H2''; cbn; auto.
             * inv H. destruct l0; cbn in *; inv H1; auto; rewrite CopySymbols_Fun_equation; cbn; rewrite H2''; cbn; auto.
       }
@@ -230,7 +230,7 @@ End Test.
 
   Function CopySymbols_TermTime (t : tape sig) { measure rlength t } : nat :=
     match t with
-    | midtape ls m rs => if f m then 8 else 8 + CopySymbols_TermTime (tape_move_right t)
+    | midtape ls m rs => if stop m then 8 else 8 + CopySymbols_TermTime (tape_move_right t)
     | _ => 8
     end.
   Proof.
@@ -239,7 +239,7 @@ End Test.
 
 
   Lemma CopySymbols_terminates :
-    projT1 CopySymbols ↓ (fun tin k => k = CopySymbols_TermTime (tin[@Fin.F1])).
+    projT1 CopySymbols ↓ (fun tin k => CopySymbols_TermTime (tin[@Fin.F1]) <= k).
   Proof.
     eapply While_terminatesIn.
     1-2: eapply Realise_total; eapply M1_RealiseIn.
@@ -247,15 +247,15 @@ End Test.
       eapply functional_functionalOn. apply M1_Rel_functional.
     }
     {
-      intros tin k ->. destruct_tapes. cbn.
-      destruct h eqn:E; cbn.
-      - rewrite CopySymbols_TermTime_equation. exists [|h;h0|], false. cbn. do 2 eexists; repeat split; eauto 6; congruence.
-      - rewrite CopySymbols_TermTime_equation. exists [|h;h0|], false. cbn. do 2 eexists; repeat split; eauto 6; congruence.
-      - rewrite CopySymbols_TermTime_equation. exists [|h;h0|], false. cbn. do 2 eexists; repeat split; eauto 6; congruence.
-      - destruct (f e) eqn:E2; cbn.
-        + rewrite CopySymbols_TermTime_equation. exists [|h; tape_write h0 (Some (g e))|], false. cbn. rewrite E2.
-          do 2 eexists; repeat split; eauto 7; try congruence.
-        + rewrite CopySymbols_TermTime_equation. exists [|tape_move_right h; tape_move_mono h0 (Some (g e), R)|], true. cbn. rewrite E2.
+      intros tin k Hk. destruct_tapes. cbn.
+      destruct h eqn:E; rewrite CopySymbols_TermTime_equation in *; cbn in *.
+      - exists [|h;h0|], false. cbn. do 2 eexists; repeat split; eauto 6; try congruence. omega.
+      - exists [|h;h0|], false. cbn. do 2 eexists; repeat split; eauto 6; try congruence. omega.
+      - exists [|h;h0|], false. cbn. do 2 eexists; repeat split; eauto 6; try congruence. omega.
+      - destruct (stop e) eqn:E2; cbn.
+        + exists [|h; tape_write h0 (Some (g e))|], false. cbn.
+          do 2 eexists; repeat split; eauto 7; try congruence. omega.
+        + exists [|tape_move_right h; tape_move_mono h0 (Some (g e), R)|], true. cbn.
           destruct l0; rewrite E; cbn in *; do 2 eexists; repeat split; eauto 7.
     }
   Qed.
@@ -265,5 +265,106 @@ End Test.
 
   Definition CopySymbols_L := Mirror CopySymbols.
 
+
+  
+  (** * Alternative correctness statements *)
+
+
+  Lemma CopySymbols_first tltr str1 x str2 tl' tr':
+    (forall x', List.In x' str1 -> stop x' = false) ->
+    (stop x = true) ->
+    tape_local (fst tltr) = str1 ++ x :: str2 ->
+    (tl', tr') = CopySymbols_Fun tltr ->
+    tape_local tl' = x :: str2.
+  Proof.
+    intros H H0. destruct tltr as [tl tr]. intros H1 E0.
+    destruct tl as [ | r rs | l ls | ls m rs]; cbn in *.
+    1-3: rewrite CopySymbols_Fun_equation in E0; inv H1; exfalso; destruct str1; cbn in *; congruence.
+    revert rs tr ls H ls m H1 tl' tr' E0. revert str1; intros str1.
+    induction str1 as [ | s' str1' IHstr1]; (* cbn in *; *) intros.
+    - cbn in *. inv H1. rewrite CopySymbols_Fun_equation in E0. rewrite H0 in E0. inv E0. cbn. auto.
+    - rewrite CopySymbols_Fun_equation in E0. destruct (stop m) eqn:E1.
+      + inv E0. cbn in *. inv H1. enough (stop s' = false)  by congruence; eauto.
+      + cbn in H1. inv H1. cbn in *. destruct str1'; cbn in *; eapply IHstr1; eauto.
+  Qed.
+
+  Lemma CopySymbols_second tltr str1 x str2 tl' tr':
+    (forall x', List.In x' str1 -> stop x' = false) ->
+    (stop x = true) ->
+    tape_local (fst tltr) = str1 ++ x :: str2 ->
+    (tl', tr') = CopySymbols_Fun tltr ->
+    left tr' = rev (map g str1) ++ left (snd tltr).
+  Proof.
+    intros H H0. destruct tltr as [tl tr]. intros H1 E0.
+    destruct tl as [ | r rs | l ls | ls m rs]; cbn in *.
+    1-3: rewrite CopySymbols_Fun_equation in E0; inv H1; exfalso; destruct str1; cbn in *; congruence.
+    revert rs tr ls H ls m H1 tl' tr' E0. revert str1; intros str1.
+    induction str1 as [ | s' str1' IHstr1]; (* cbn in *; *) intros.
+    - cbn in *. inv H1. rewrite CopySymbols_Fun_equation in E0. rewrite H0 in E0. inv E0. cbn. auto.
+    - rewrite CopySymbols_Fun_equation in E0. destruct (stop m) eqn:E1.
+      + inv E0. cbn in *. inv H1. enough (stop s' = false)  by congruence; eauto.
+      + cbn in H1. inv H1. cbn in *.
+        simpl_list; cbn.
+        destruct str1'; cbn in *.
+        * clear H. erewrite IHstr1; eauto. destruct tr; cbn; eauto. destruct l0; cbn; auto.
+        * simpl_list; cbn.
+          erewrite IHstr1; eauto. destruct tr; simpl_list; cbn; eauto. destruct l0; cbn; auto.
+  Qed.
+
+  Definition CopySymbols_Rel' : Rel (tapes sig 2) (unit * tapes sig 2) :=
+    ignoreParam (
+        fun tin tout =>
+          forall str1 x str2,
+            (forall x', List.In x' str1 -> stop x' = false) ->
+            stop x = true ->
+            tape_local (tin[@Fin.F1]) = str1 ++ x :: str2 ->
+            tape_local (tout[@Fin.F1]) = x :: str2 /\
+            left (tout[@Fin.FS Fin.F1]) = rev (map g str1) ++ left (tin[@Fin.FS Fin.F1])
+      ).
+
+  Corollary CopySymbols_WRealise' :
+    CopySymbols ⊫ CopySymbols_Rel'.
+  Proof.
+    eapply WRealise_monotone. eapply CopySymbols_WRealise.
+    hnf. intros tin (? & tout). destruct_tapes. cbn. intros H.
+    hnf. intros str1 str2 x Hstr1 Hx HEnc. split.
+    - eapply CopySymbols_first; cbn; eauto. cbn. eauto.
+    - epose proof CopySymbols_second (tltr := (_, _)) Hstr1 Hx as L. spec_assert L; cbn; eauto.
+  Qed.
+
+  Lemma CopySymbols_TermTime_local t r1 sym r2 :
+    tape_local t = r1 ++ sym :: r2 ->
+    stop sym = true ->
+    CopySymbols_TermTime t <= 8 + 8 * length r1.
+  Proof.
+    revert t sym r2. induction r1; intros t sym r2 HEnc HStop; cbn -[plus mult] in *.
+    - destruct t; cbn in HEnc; inv HEnc. rewrite CopySymbols_TermTime_equation. rewrite HStop. cbn. omega.
+    - destruct t; cbn in HEnc; try congruence. inv HEnc.
+      rewrite CopySymbols_TermTime_equation. destruct (stop a).
+      + omega.
+      + apply Nat.add_le_mono_l. replace (8 * S (|r1|)) with (8 + 8 * |r1|) by omega.
+        eapply IHr1; eauto. cbn. now simpl_tape.
+  Qed.
+
+
+  Corollary CopySymbols_terminates' :
+    projT1 CopySymbols ↓
+           (fun tin k => exists r1 r2 sym, tape_local (tin[@Fin.F1]) = r1 ++ sym :: r2 /\ stop sym = true /\ 8 + 8 * length r1 <= k).
+  Proof.
+    eapply TerminatesIn_monotone. eapply CopySymbols_terminates.
+    intros tin k. intros (r1&r2&x&Hx&HEnc&Hk). rewrite <- Hk. eapply CopySymbols_TermTime_local; eauto.
+  Qed.
+
   
 End CopySymbols.
+
+Arguments CopySymbols : simpl never.
+Arguments CopySymbols_L : simpl never.
+
+Ltac smpl_TM_CopySymbols :=
+  match goal with
+  | [ |- CopySymbols  _ _ ⊫ _ ] => eapply CopySymbols_WRealise'
+  | [ |- projT1 (CopySymbols _ _) ↓ _ ] => eapply CopySymbols_terminates'
+  end.
+
+Smpl Add smpl_TM_CopySymbols : TM_Correct.
