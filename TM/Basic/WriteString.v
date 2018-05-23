f362dad5042e9c30d554d446dec01f152f196693
Maximilian Wuttke <mwuttke97@gmail.com>
2018-05-23 21:04:10 +0200

    Reimplement WriteValue

--- TM/Basic/WriteString.v
+++ TM/Basic/WriteString.v
@@ -1,59 +1,69 @@
-Require Import TM.TM TM.Basic.Mono TM.Combinators.SequentialComposition.
+Require Import TM.TM TM.Basic.Mono TM.Combinators.Combinators.
 Require Import List.
+Require Import TMTac.
 
+Require Coq.derive.Derive.
+
+(* Useful for runtime stuff *)
+Arguments plus : simpl never. Arguments mult : simpl never.
+
+
+(** The correctness and definition of [WriteString] is uncommon, because it is defined (and verified) by recursion (or induction) over the string *)
 Section Write_String.
 
   Variable sig : finType.
   Variable D : move.
-  Variable (F : finType) (f : F).
 
-  Fixpoint Write_String (l : list sig) : {M : mTM sig 1 & states M -> F} :=
+  Fixpoint WriteString (l : list sig) : {M : mTM sig 1 & states M -> unit} :=
     match l with
-    | [] => mono_Nop sig f
-    | x :: xs => Write x tt ;; Move _ D tt ;; Write_String xs
+    | [] => mono_Nop sig tt
+    | x :: xs => WriteMove (Some x, D) tt ;; WriteString xs
     end.
 
-  Fixpoint Tape_Write_String (t : tape sig) (str : list sig) :=
+  Fixpoint WriteString_Fun (t : tape sig) (str : list sig) :=
     match str with
     | nil => t
-    | s :: str' => Tape_Write_String (tape_move_mono t (Some s, D)) str'
+    | s :: str' => WriteString_Fun (tape_move_mono t (Some s, D)) str'
     end.
   
-  Lemma Tape_Write_String_nil (t : tape sig) :
-    Tape_Write_String t nil = t.
+  Lemma Write_String_nil (t : tape sig) :
+    WriteString_Fun t nil = t.
   Proof. destruct t; cbn; auto. Qed.
 
-  Fixpoint Write_string_sem_fix (str : list sig) : Rel (tapes sig 1) (F * tapes sig 1) :=
+  Fixpoint WriteString_sem_fix (str : list sig) : Rel (tapes sig 1) (unit * tapes sig 1) :=
     match str with
-    | nil => mono_Nop_R f
-    | s :: str' => Write_R s tt ∘ hideParam (Move_R D tt ∘ hideParam (Write_string_sem_fix str'))
+    | nil => mono_Nop_R tt
+    | s :: str' =>
+      WriteMove_R (Some s, D) tt |_tt ∘ WriteString_sem_fix str'
     end.
     
-  Lemma Write_string_fix_Sem (str : list sig) :
-    Write_String str ⊨c(4 * |str|) (Write_string_sem_fix str).
+  Lemma WriteString_fix_Sem (str : list sig) :
+    WriteString str ⊨c(3 * |str|) (WriteString_sem_fix str).
   Proof.
-    induction str.
-    - cbn. apply mono_Nop_Sem.
-    - cbn.
-      replace (S ((| str |) + S ((| str |) + S ((| str |) + S ((| str |) + 0)))))
-        with (1 + 1 + (1 + 1 + (4 * (|str|)))); [ | cbn; omega].
-      apply Seq_RealiseIn.
-      + apply Write_Sem.
-      + apply Seq_RealiseIn.
-        * apply Move_Sem.
-        * apply IHstr.
+    induction str; cbn; eapply RealiseIn_monotone; cbn.
+    - repeat TM_Correct.
+    - omega.
+    - firstorder.
+    - repeat TM_Correct.
+    - omega.
+    - intros tin (y, tout) H. TMSimp. exists tmid. repeat split; auto. 
   Qed.
   
-  Definition Write_String_R str :=
-    Mono.Mk_R_p (fun tin '(yout, tout) => tout = Tape_Write_String tin str /\ yout = f).
-  Lemma Write_string_Sem str :
-    Write_String str ⊨c(4 * |str|) (Write_String_R str).
+  Definition WriteString_Rel str : Rel (tapes sig 1) (unit * tapes sig 1) :=
+    Mono.Mk_R_p (ignoreParam (fun tin tout => tout = WriteString_Fun tin str)).
+
+  Lemma WriteString_Sem str :
+    WriteString str ⊨c(3 * |str|) (WriteString_Rel str).
   Proof.
-    eapply RealiseIn_monotone with (k1 := 4 * (|str|)); [now apply Write_string_fix_Sem | omega | ]. 
-    induction str.
-    - hnf. intros x (f', y) (H1 & H2). hnf in *. subst. now rewrite Tape_Write_String_nil.
-    - intros t1 (f', t2) (([]&t3)&H1&((b&t4)&H2&H3)). hnf in *. destruct H1 as [_ H1]. destruct H2 as [_ H2]. hnf in *.
-      destruct_tapes. cbn in *. cbn. subst. specialize (IHstr _ _ H3). clear H3. cbn in IHstr. hnf in IHstr. auto.
+    eapply RealiseIn_monotone with (k1 := 3 * (|str|)).
+    - apply WriteString_fix_Sem.
+    - reflexivity.
+    - induction str as [ | s str' IH]; intros tin (yout, tout) H; cbn in *.
+      + now TMSimp.
+      + TMSimp; clear_trivial_eqs. hnf in IH. specialize IH with (1 := H0). now TMSimp.
   Qed.
 
 End Write_String.
+
+Arguments WriteString : simpl never.
+Arguments WriteString_Rel {sig} (D) (str) x y/.
---
+++ TM/Code/WriteValue.v
@@ -0,0 +1,43 @@
+Require Import CodeTM.
+Require Import TM.Basic.WriteString.
+Require Import Basic.Mono.
+
+
+Lemma WriteString_L_right (sig : finType) (str : list sig) t :
+  right (WriteString_Fun L t str) = rev str ++ right t.
+Proof.
+  revert t. induction str; intros; cbn in *.
+  - reflexivity.
+  - rewrite IHstr. simpl_tape. rewrite <- app_assoc. reflexivity.
+Qed.
+
+Arguments WriteString_Fun : simpl never.
+
+Section WriteValue.
+
+  Variable (sig: finType) (X: Type) (cX: codable sig X).
+
+  Definition WriteValue (x:X) : { M : mTM sig^+ 1 & states M -> unit } :=
+    WriteString L (inl STOP :: rev (encode x));; Write (inl START) tt.
+
+  Definition WriteValue_Rel (x : X) : Rel (tapes sig^+ 1) (unit * tapes sig^+ 1) :=
+    Mk_R_p (ignoreParam (fun tin tout => isRight tin -> tout ≃ x)).
+
+
+  Lemma WriteValue_Sem (x : X) :
+    WriteValue x ⊨c(5 + 3 * length (cX x)) WriteValue_Rel x.
+  Proof.
+    eapply RealiseIn_monotone.
+    { unfold WriteValue. repeat TM_Correct. eapply WriteString_Sem. }
+    { cbn. rewrite rev_length, map_length. apply Nat.eq_le_incl. omega. }
+    {
+      intros tin ((), tout) H. intros HRight.
+      TMSimp; clear_trivial_eqs.
+      repeat econstructor. f_equal. rewrite WriteString_L_right. cbn.
+      rewrite <- app_assoc.
+      rewrite rev_involutive.
+      rewrite isRight_right; auto.
+    }
+  Qed.
+
+End WriteValue.
