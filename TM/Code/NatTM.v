5a27420fe50f0ca66dfda032ab413aae98914483
Maximilian Wuttke <mwuttke97@gmail.com>
2018-05-25 15:10:17 +0200

    Reimplement Runtime of Add and Mult

--- TM/Code/NatTM.v
+++ TM/Code/NatTM.v
@@ -223,17 +223,15 @@ Qed.
 
 (** ** Termination of [Add] *)
 
-(* TODO *)
-
-(*
+Local Arguments plus : simpl never. Local Arguments mult : simpl never.
 
 Definition Add_Loop_steps b := 11 + 12 * b.
 
 Lemma Add_Loop_Terminates :
   projT1 Add_Loop ↓
          (fun tin i => exists a b,
-              tin[@Fin0] ≂ a /\
-              tin[@Fin1] ≂ b /\
+              tin[@Fin0] ≃ a /\
+              tin[@Fin1] ≃ b /\
               Add_Loop_steps b <= i).
 Proof.
   unfold Add_Loop, Add_Loop_steps. repeat TM_Correct.
@@ -241,90 +239,82 @@ Proof.
   { eapply RealiseIn_terminatesIn. apply Add_Step_Sem. }
   {
     intros tin i (a&b&HEncA&HEncB&Hi).
-    destruct HEncB as (r1 & r2 & HEncB % tape_encodes_l_encodes_size).
     destruct b.
     - exists 11. repeat split.
       + omega.
-      + intros y () tmid H. cbn in H. specialize (H _ _ _ _ HEncA HEncB). cbn in *.
+      + intros y () tmid H. cbn in H. specialize (H _ _ HEncA HEncB). cbn in *.
         destruct H; TMSimp; inv_pair. omega.
     - exists 11. repeat split.
       + omega.
-      + intros y () tmid H. cbn in H. specialize (H _ _ _ _ HEncA HEncB). cbn -[plus mult] in *.
+      + intros y () tmid H. cbn in H. specialize (H _ _ HEncA HEncB). cbn -[plus mult] in *.
         destruct H as (H1&H2&H3). inv H3.
         exists (11 + b * 12). repeat split.
-        * do 2 eexists. repeat split. eauto. eapply tape_encodes_size_encodes; eauto. omega.
+        * do 2 eexists. repeat split; eauto. omega.
         * omega.
   }
 Qed.
 
 
-Definition Add_Main_steps m n := 101 + 16 * n + 38 * m.
+Definition Add_Main_steps m n := 87 + 12 * n + 24 * m.
+(* [37 + 12 * n] for [CopyValue] (n) *)
+(* [37 + 12 * m] for [CopyValue] (m) *)
+(* [11 + 12 * m] for [Add_Main] *)
 
 Lemma Add_Main_Terminates :
   projT1 Add_Main ↓ Computes2_T Add_Main_steps.
 Proof.
-  Arguments add : simpl never. Arguments mult : simpl never.
   unfold Add_Main, Add_Main_steps. eapply TerminatesIn_monotone.
   {
     repeat TM_Correct.
-    - apply CopyValue'_Realise with (X := nat).
-    - apply CopyValue'_Terminates with (X := nat).
-    - apply CopyValue'_Realise with (X := nat).
-    - apply CopyValue'_Terminates with (X := nat).
+    - apply CopyValue_Realise with (X := nat).
+    - apply CopyValue_Terminats with (X := nat).
+    - apply CopyValue_Realise with (X := nat).
+    - apply CopyValue_Terminats with (X := nat).
     - apply Add_Loop_Terminates.
   }
   {
-    intros tin k (m&n&HEncM&HEncN&HInt&Hk).
-    pose proof HEncN as (r11&r12&HEncN').
-    pose proof HEncM as (r21&r22&HEncM').
-    exists (44 + 16 * n), (56 + 28 * m). repeat split.
+    intros tin k (m&n&HEncM&HEncN&HOut&HInt&Hk).
+    exists (37 + 12 * n), (49 + 24 * m). repeat split.
     - rewrite <- Hk. omega.
-    - cbn -[add mult]. exists n. rewrite nat_encode_length. split; auto. omega.
+    - cbn. exists n. rewrite app_length,repeat_length; cbn. split; auto. omega.
     - intros tmid ymid. intros (H1&H2). TMSimp.
-      specialize (H1 _ _ _ HEncN'). TMSimp.
+      specialize (H1 _ HEncN HOut). TMSimp.
       specialize (HInt Fin0).
-      exists (44 + 16 * m), (Add_Loop_steps m). repeat split.
+      exists (37 + 12 * m), (Add_Loop_steps m). repeat split.
       + unfold Add_Loop_steps. omega.
-      + exists m. split. eauto. rewrite nat_encode_length. omega.
+      + exists m. split. eauto. rewrite app_length, repeat_length; cbn. omega.
       + intros tmid2 () (HComp & HInj). TMSimp.
-        specialize (HComp _ _ _ HEncM') as (HComp&HComp').
+        specialize (HComp _ HEncM HInt) as (HComp&HComp').
         do 2 eexists; repeat split; eauto; do 2 eexists; eassumption.
   }
 Qed.
 
 
-Definition Add_steps m n := 120 + 16 * n + 42 * m.
+Definition Add_steps m n := 100 + 12 * n + 24 * m.
+(* Additional [12] steps for [Reset], and [1] for [Seq] *)
 
 Lemma Add_Terminates :
   projT1 Add ↓ Computes2_T Add_steps.
 Proof.
-  Arguments add : simpl never. Arguments mult : simpl never.
   unfold Add, Add_steps. eapply TerminatesIn_monotone.
   {
     repeat TM_Correct.
     - apply Add_Main_Realise.
     - apply Add_Main_Terminates.
-    - apply MoveToLeft_Terminates.
+    - apply MoveRight_Terminates with (X := nat).
   }
   {
-    intros tin k (m&n&HEncM&HEncN&HInt&Hk).
-    pose proof HEncM as (r11&r12&HEncM'); pose proof HEncN as (r21&r22&HEncN').
-    exists (Add_Main_steps m n), (18 + 4 * m). repeat split.
+    intros tin k (m&n&HEncM&HEncN&HOut&HInt&Hk).
+    exists (Add_Main_steps m n), 12. repeat split.
     - unfold Add_Main_steps. omega.
     - cbn. exists m, n. repeat split; eauto.
     - intros tmid () HComp. cbn in *.
-      specialize (HInt Fin0). apply isLeft_isLeft_size in HInt.
-      specialize (HComp _ _ _ HEncM HEncN HInt) as (HComp1&HComp2&HComp3&HComp4).
-      destruct HComp4 as (r31&r32&Hr31&_&HComp4).
-      do 3 eexists. split; eauto. rewrite nat_encode_length.
-      transitivity (14 + 4 * (m + 1)).
-      + apply Nat.add_le_mono. omega. apply Nat.mul_le_mono. omega. apply Nat.add_le_mono. assumption. omega.
-      + omega.
+      specialize (HInt Fin0).
+      specialize (HComp _ _ HEncM HEncN HOut HInt) as (HComp1&HComp2&HComp3&HComp4).
+      exists 0. split. eauto. rewrite app_length, repeat_length. cbn. omega.
   }
 Qed.
 
-
-*)
 
 
 (** * Multiplication *)
@@ -491,7 +481,7 @@ Proof.
       destruct m' as [ | m']; TMSimp; inv_pair; try congruence.
       specialize (HLastStep _ _ _ ltac:(eauto) ltac:(eauto) ltac:(eauto) ltac:(eauto) ltac:(eauto)) as (HL1&HL2&HL3&HL4&HL).
       rewrite Nat.add_assoc in HL3. replace (n + m' * n + c) with (m' * n + n + c) by omega.
-      repeat split; auto.
+      repeat split; auto. apply tape_contains_ext with (1 := HL3). f_equal. rewrite Nat.mul_succ_l. omega.
   }
 Qed.
 
@@ -576,32 +566,32 @@ Proof.
 Qed.
 
 
-(*
 (* TODO *)
 
 (** ** Termination of Mult *)
 
-Print Mult_Step.
-
-Check CopyValue'_Terminates.
-
 Definition Mult_Step_steps m' n c :=
   match m' with
   | O => 6
-  | _ => 190 + 62 * n + 36 * c
+  | _ => 170 + 33 * c + 41 * n
   end.
+(* [5] for [If] and [1] for [MatchNat] *)
+(* [100+12*n+24*c] for [Add_Main] *)
+(* [12+c] for [Reset] (c) *)
+(* [36+12*(c+n)] for [CopyValue] (c' = c + n) *)
+(* [12 + (c+n)] for [Reset] (c' = c + n) *)
+
 
 Lemma Mult_Step_Terminates :
   projT1 Mult_Step ↓
          (fun tin k => exists m' n c,
-              tin[@Fin0] ≂ m' /\
-              tin[@Fin1] ≂ n /\
-              tin[@Fin2] ≂ c /\
-              isLeft tin[@Fin3] /\
-              isLeft tin[@Fin4] /\
+              tin[@Fin0] ≃ m' /\
+              tin[@Fin1] ≃ n /\
+              tin[@Fin2] ≃ c /\
+              isRight tin[@Fin3] /\
+              isRight tin[@Fin4] /\
               Mult_Step_steps m' n c <= k).
 Proof.
-  Local Arguments add : simpl never. Local Arguments mult : simpl never.
   eapply TerminatesIn_monotone.
   {
     unfold Mult_Step. repeat TM_Correct.
@@ -609,48 +599,142 @@ Proof.
     - eapply RealiseIn_terminatesIn. apply MatchNat_Sem.
     - apply Add_Computes.
     - apply Add_Terminates.
-    - apply CopyValue'_Realise with (X := nat).
-    - apply CopyValue'_Terminates with (X := nat).
-    - apply MoveToLeft_Terminates with (X := nat).
+    - apply Reset_Realise with (X := nat).
+    - apply MoveRight_Terminates with (X := nat).
+    - apply CopyValue_Realise with (X := nat).
+    - apply CopyValue_Terminats with (X := nat).
+    - apply MoveRight_Terminates with (X := nat).
   }
   {
     intros tin k. intros (m'&n&c&HEncM'&HEncN&HEncC&HInt3&HInt4&Hk).
-    pose proof HEncM' as (r11&r12&HEncM'').
     destruct m' as [ | m']; cbn.
     - exists 5, 0. cbn in *; repeat split.
       + omega.
       + omega.
       + intros tmid y (HComp&HInj). TMSimp.
-        rewrite <- HInj0, <- HInj1, <- HInj2, <- HInj3 in *. clear HInj0 HInj1 HInj2 HInj3.
-        specialize (HComp _ _ _ HEncM''). cbn in *. destruct HComp as (HComp&->). omega.
-    - exists 5, (184 + 62 * n + 36 * c); cbn in *; repeat split.
+        specialize (HComp _ HEncM'). cbn in *. destruct HComp as (HComp&->). omega.
+    - exists 5, (164 + 33 * c + 41 * n); cbn in *; repeat split.
       + omega.
       + omega.
       + intros tmid y (HComp&HInj). TMSimp.
-        rewrite <- HInj0, <- HInj1, <- HInj2, <- HInj3 in *. clear HInj0 HInj1 HInj2 HInj3.
-        specialize (HComp _ _ _ HEncM''). cbn in *. destruct HComp as (HComp&->).
-        exists (120 + 42 * n + 16 * c), (63 + 20 * n + 20 * c); cbn in *; repeat split.
-        * omega.
+        specialize (HComp _ HEncM'). cbn in *. destruct HComp as (HComp&->).
+        exists (Add_steps n c), (63 + 21 * c + 17 * n); cbn in *; repeat split.
+        * unfold Add_steps. omega.
         * do 2 eexists. repeat split; eauto.
-          -- intros i; destruct_fin i; cbn. eauto.
-          -- unfold Add_steps. omega.
-        * intros tmid0 () (HComp2&HInj). TMSimp. rewrite <- HInj0 in *. clear HInj0.
-          specialize (HComp2 _ _ ltac:(eauto) ltac:(eauto)).
-          spec_assert HComp2 as (HComp1&HComp2&HComp3&HComp4).
-          { intros i; destruct_fin i; cbn; eauto. }
-          specialize (HComp4 Fin0). cbn in HComp4.
-          pose proof HComp3 as (r21&r22&HComp3').
-          exists (44 + 16 * n + 16 * c), (18 + 4 * n + 4 * c). repeat split.
+          intros i; destruct_fin i; cbn. eauto.
+        * intros tmid0 () (HComp2&HInj). TMSimp.
+          specialize HComp2 with (1 := HEncN) (2 := HEncC) (3 := HInt3).
+          spec_assert HComp2 as (HComp2&HComp3&HComp4&HComp5) by (intros i; destruct_fin i; cbn; auto).
+          specialize (HComp5 Fin0). cbn in *. TMSimp.
+          exists (12 + 4 * c), (50 + 17 * (c + n)). repeat split.
           -- omega.
-          -- eexists. repeat split; eauto. rewrite nat_encode_length. omega.
-          -- intros tmid1 () (HComp5&HInj). TMSimp. clear HInj0 HInj1 HInj2.
-             specialize HComp5 with (1 := HComp3') as (->&HComp5).
-             do 3 eexists. split. eauto. rewrite nat_encode_length.
-             rewrite Nat.mul_add_distr_l, Nat.mul_succ_r, Nat.mul_add_distr_l, !Nat.add_assoc.
-             enough (| r21 | <= 0) by omega.
-             (* Problemma *)
-             admit.
+          -- eexists. repeat split. eauto. rewrite app_length, repeat_length; cbn. omega.
+          -- intros tmid1 () (HComp6&HInj). TMSimp.
+             specialize HComp6 with (1 := HComp3).
+             exists (37 + 12 * (c + n)), (12 + 4 * (c + n)). repeat split.
+             ++ omega.
+             ++ eexists. repeat split. eauto. rewrite app_length, repeat_length; cbn. omega.
+             ++ intros tmid2 () (HComp7&HInj7); TMSimp. specialize HComp7 with (1 := HComp4) (2 := HComp6) as (HComp7&HComp8).
+                eexists. repeat split. eauto. rewrite app_length, repeat_length; cbn. omega.
   }
-Admitted.
+Qed.
 
-*)
+
+Fixpoint Mult_Loop_steps m' n c :=
+  match m' with
+  | O => S (Mult_Step_steps m' n c)
+  | S m'' => S (Mult_Step_steps m' n c) + Mult_Loop_steps m'' n (n + c)
+  end.
+
+
+Lemma Mult_Loop_Terminates :
+  projT1 Mult_Loop ↓
+         (fun tin i => exists m' n c,
+              tin[@Fin0] ≃ m' /\
+              tin[@Fin1] ≃ n /\
+              tin[@Fin2] ≃ c /\
+              isRight tin[@Fin3] /\
+              isRight tin[@Fin4] /\
+              Mult_Loop_steps m' n c <= i).
+Proof.
+  unfold Mult_Loop. repeat TM_Correct.
+  { apply Mult_Step_Realise. }
+  { apply Mult_Step_Terminates. }
+  {
+    intros tin k (m'&n&c&HEncM'&HEncN&HEncC&HRight3&HRight4&Hk).
+    destruct m' as [ | m''] eqn:E; cbn in *; exists (Mult_Step_steps m' n c).
+    {
+      repeat split.
+      - do 3 eexists. repeat split; eauto. cbn. unfold Mult_Step_steps. destruct m'; omega.
+      - intros b () tmid H1.
+        specialize H1 with (1 := HEncM') (2 := HEncN) (3 := HEncC) (4 := HRight3) (5 := HRight4) as
+            (HComp1&HComp2&HComp3&HComp4&HComp5&H'); inv H'.
+        cbn. omega.
+    }
+    {
+      repeat split.
+      - do 3 eexists. repeat split; eauto. cbn. unfold Mult_Step_steps. destruct m'; omega.
+      - intros b () tmid H1.
+        specialize H1 with (1 := HEncM') (2 := HEncN) (3 := HEncC) (4 := HRight3) (5 := HRight4) as
+            (HComp1&HComp2&HComp3&HComp4&HComp5&H'); inv H'.
+        cbn. eexists. repeat split.
+        + do 3 eexists. repeat split; eauto.
+        + cbn. rewrite <- Hk. clear_all. omega.
+    }
+  }
+Qed.
+
+
+Definition Mult_Main_steps m n := 44 + 12 * m + Mult_Loop_steps m n 0.
+(* [2] steps for [Seq], in total *)
+(* [37+12*m] for [CopyValue] (m) *)
+(* [Mult_Loop_steps m n 0] for [Mult_Loop] *)
+
+
+
+Lemma Mult_Main_Terminates : projT1 Mult_Main ↓ Computes2_T Mult_Main_steps.
+Proof.
+  eapply TerminatesIn_monotone.
+  { unfold Mult_Main. repeat TM_Correct.
+    - apply CopyValue_Realise with (X := nat).
+    - apply CopyValue_Terminats with (X := nat).
+    - eapply RealiseIn_Realise. apply Constr_O_Sem.
+    - eapply RealiseIn_terminatesIn. apply Constr_O_Sem.
+    - apply Mult_Loop_Terminates.
+  }
+  {
+    intros tin k (m&n&HEncM&HEncN&HOut&HInt&Hk). cbn in *. unfold Mult_Main_steps in Hk.
+    exists (37 + 12 * m), (6 + Mult_Loop_steps m n 0). repeat split.
+    - omega.
+    - eexists. repeat split; eauto. rewrite app_length, repeat_length; cbn. omega.
+    - intros tmid () (H1&H2); TMSimp. specialize H1 with (1 := HEncM) (2 := HInt _) as (H1&H1').
+      exists 5, (Mult_Loop_steps m n 0). repeat split.
+      + omega.
+      + omega.
+      + intros tmid2 () (H2&HInj2); TMSimp. specialize H2 with (1 := HOut).
+        do 3 eexists. repeat split; eauto.
+  }
+Qed.
+
+
+Definition Mult_steps m n := 13 + Mult_Main_steps m n.
+
+Lemma Mult_Terminates : projT1 Mult ↓ Computes2_T Mult_steps.
+Proof.
+  eapply TerminatesIn_monotone.
+  { unfold Mult. repeat TM_Correct.
+    - apply Mult_Main_Realise.
+    - apply Mult_Main_Terminates.
+    - apply MoveRight_Terminates with (X := nat).
+  }
+  {
+    intros tin k (m&n&HEncM&HEncN&HOut&HInt&Hk). cbn in *. unfold Mult_steps in Hk.
+    exists (Mult_Main_steps m n), 12. repeat split.
+    - omega.
+    - do 2 eexists; repeat split; eauto.
+    - intros tmid () H1; TMSimp.
+      specialize H1 with (1 := HEncM) (2 := HEncN) (3 := HOut) (4 := HInt _) (5 := HInt _) (6 := HInt _)
+        as (H1&H2&H3&H4&H5&H6).
+      exists 0. split; auto.
+  }
+Qed.
