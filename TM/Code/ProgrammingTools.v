7a0d970633941ab5e9d6dda5a3ced086fbae5d85
Maximilian Wuttke <mwuttke97@gmail.com>
2018-06-10 18:11:27 +0200

    Add missing ProgrammingTools.v, which containins the tactic modpon

---
+++ TM/Code/ProgrammingTools.v
@@ -0,0 +1,30 @@
+Require Export CodeTM Copy ChangeAlphabet WriteValue.
+Require Export TMTac.
+Require Export Basic.Mono.
+
+
+(** This tactic automatically solves/instantiates hypothesises. *)
+
+Ltac modpon_step H :=
+  match type of H with
+  | forall (x : ?X), ?P =>
+    match type of X with
+    | Prop => spec_assert H; simpl_surject;
+          [ now eauto
+          | TMSimp simpl_surject
+          ]
+    | _ => let x' := fresh "x" in
+          evar (x' : X);
+          specialize (H x');
+          subst x'
+    end
+  end.
+
+Ltac modpon H := repeat modpon_step H.
+
+
+
+(** Machine Notations *)
+
+Notation "M @ t" := (M, t, t) (at level 41, only parsing).
+Notation "pM ⇑ R" := (ChangeAlphabet pM R) (at level 40, only parsing).
